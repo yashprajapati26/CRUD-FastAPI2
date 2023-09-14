2cf19f70cb37565d0f5d955ff83778c69b3d0dd4
yashprajapati26 <yash260801@gmail.com>
2023-09-14 11:34:59 +0530

    some changes

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 import models, schemas
 
-
+# for users
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
@@ -13,7 +13,6 @@ def get_user_by_email(db: Session, email: str):
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-
 def create_user(db: Session, user: schemas.User):
     db_user = models.User(email=user.email, password=user.password)
     db.add(db_user)
@@ -21,14 +20,45 @@ def create_user(db: Session, user: schemas.User):
     db.refresh(db_user)
     return db_user
 
+def update_user(db:Session, user:schemas.User, user_id:int):
+    user_to_update = db.query(models.User).filter_by(id=user_id).first()
+    if user_to_update:
+        user_to_update.email = user.email
+        user_to_update.password = user.password
+        db.commit()
+        return user_to_update
+   
+    
+def delete_user(db:Session, user_id:int):
+    user_to_delete = db.query(models.User).filter_by(id=user_id).first()
+    if user_to_delete:
+        db.delete(user_to_delete)
+        db.commit()
+        return True
+
+
+
+# for items 
+def get_item(db: Session, item_id: int):
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
-
-def create_user_item(db: Session, item: schemas.Item, user_id: int):
-    db_item = models.Item(title=item.title, description=item.description, owner_id=user_id)
+def create_item(db: Session, item: schemas.Item):
+    db_item = models.Item(title=item.title, description=item.description, owner_id=item.owner_id)
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
     return db_item
+
+def update_item(db:Session, item:schemas.Item, item_id:int):
+    item_to_update = db.query(models.Item).filter_by(id=item_id).first()
+    if item_to_update:
+        item_to_update.title = item.title
+        item_to_update.description = item.description
+        item_to_update.owner_id = item.owner_id
+        db.commit()
+        return item_to_update
+    else:
+        return False
--- main.py
+++ main.py
@@ -11,8 +11,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-
-
 # Dependency
 def get_db():
     db = SessionLocal()
@@ -21,6 +19,7 @@ def get_db():
     finally:
         db.close()
 
+# users routes 
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
@@ -29,12 +28,10 @@ def create_user(user: schemas.User, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
-
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
-
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
@@ -43,14 +40,46 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
+@app.put("/users/{user_id}",response_model=schemas.User)
+def update_user(user_id: int, user: schemas.User,db: Session = Depends(get_db)):
+    updated_user = crud.update_user(db, user_id=user_id, user=user)
+    if updated_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return updated_user
 
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(user_id: int, item: schemas.Item, db: Session = Depends(get_db)):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    delete_user = crud.delete_user(db, user_id=user_id)
+    if delete_user:
+        return {"msg": "User Deleted Sucessfully"}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+
+# item routes
+
+@app.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.Item, db: Session = Depends(get_db)):
+    return crud.create_item(db=db, item=item)
 
 
 @app.get("/items/", response_model=List[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+@app.get("/items/{item_id}", response_model=schemas.Item)
+def read_item(item_id :int , db:Session = Depends(get_db)):
+    item = crud.get_item(db, item_id=item_id)
+    return item
+
+@app.put("/items/{item_id}",response_model=schemas.Item)
+def update_item(item_id: int, item: schemas.Item,db: Session = Depends(get_db)):
+    updated_item = crud.update_item(db, item_id=item_id, item=item)
+    if updated_item:
+        return updated_item
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
 
